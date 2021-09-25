7e5f58cc68f9494371c70fba9c143a0e488f904b
PrOF-kk <valerio.colella.2001@gmail.com>
2021-09-25 20:48:46 +0200

    Overhaul script, remove all script state persistence

--- Skins/Keep/Keep.py
+++ Skins/Keep/Keep.py
@@ -10,7 +10,6 @@ import configparser
 from gkeepapi.node import TopLevelNode
 
 # Constants
-SCRIPT_STATE_FILE: str = "./script_state"
 NOTES_CACHE_FILE: str = "./cache"
 
 config = configparser.ConfigParser()
@@ -24,85 +23,21 @@ APP_PASSWORD: str = "APP PASSWORD, see https://support.google.com/accounts/answe
 
 def main():
     func = sys.argv[1]
-    state = initialize()
+    
+    # if first time:
+    keep = gkeepapi.Keep()
+    keep.login(USER_EMAIL, APP_PASSWORD)
+    #TODO state cache
+    #TODO else load master token and resume
 
     if func == "get":
-        note = getNote(state, NOTE_ID)
+        note = keep.get(NOTE_ID)
         print(note.text)
-    elif func == "cache":
-        saveNoteCache(state.keep)
     elif func == "title":
-        note = getNote(state, NOTE_ID)
+        note = keep.get(NOTE_ID)
         print(note.title)
     else:
-        print("Unimplemented command, exiting")
-
-class State:
-    '''Class representing data that should persist between script launches'''
-    keep: gkeepapi.Keep = None
-    username: str = None
-    # This is unsafe, use a keyring in the future
-    masterToken: str = None
-
-
-def initialize() -> State:
-    '''Initializes script for first-time run, or restores previous script state'''
-    if os.path.isfile(SCRIPT_STATE_FILE):
-        # State file found. Resume Keep object and return state
-        return getScriptState()
-    else:
-        # First time setup
-        return doFirstTimeSetup()
-
-def doFirstTimeSetup() -> State:
-    state = State()
-    state.keep = gkeepapi.Keep()
-    state.username = USER_EMAIL
-    loginKeep(state, APP_PASSWORD)
-    state.masterToken = state.keep.getMasterToken()
-    #saveScriptState(state)
-    return state
-
-# Note handling
-def getNote(state: State, noteId: str) -> TopLevelNode:
-    return state.keep.get(noteId)
-
-# Script state
-def saveScriptState(state: State):
-    '''Saves the current script state to json'''
-    with open(SCRIPT_STATE_FILE, 'w+') as stateFile:
-        json.dump(state, stateFile)
-
-def getScriptState() -> State:
-    '''Returns the script state from json file'''
-    with open(SCRIPT_STATE_FILE) as stateFile:
-        return json.load(stateFile)
-
-# Keep state methods
-def resumeKeep(state: State):
-    k = state.keep
-    k.resume(state.username, state.masterToken)
-
-def resumeKeepCache(state: State):
-    k = state.keep
-    k.resume(state.username, state.masterToken, getNoteCache())
-
-def loginKeep(state: State, password: str):
-    k = state.keep
-    k.login(state.username, password)
-
-def loginKeepCache(state: State, password: str):
-    k = state.keep
-    k.login(state.username, password, getNoteCache())
-
-# Note cache methods
-def getNoteCache():
-    with open(NOTES_CACHE_FILE, 'r') as cache:
-        return json.load(cache)
-
-def saveNoteCache(keep: gkeepapi.Keep):
-    with open(NOTES_CACHE_FILE, 'w+') as cache:
-        json.dump(keep.dump(), cache)
+        print("Unimplemented command.")
 
 if __name__ == "__main__":
     main()
