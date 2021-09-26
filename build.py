9ddf6f318ccc20b35983c6c7fd4f0ddb3b159e0b
PrOF-kk <valerio.colella.2001@gmail.com>
2021-09-26 11:08:29 +0200

    Build script: overwrite installed settings file with debug ones

--- build.py
+++ build.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2021 Valerio Colella
 # SPDX-License-Identifier: MIT
 
-# Build script: move everything in the correct Rainmeter Skins folder for testing
-
 import shutil
 import os
+import configparser
 
-print(shutil.copytree("Skins\\Keep", os.environ["USERPROFILE"] + "\\Documents\\Rainmeter\\Skins\\Keep\\", dirs_exist_ok=True))
+SOURCE = "Skins\\Keep"
+INSTALL_LOCATION = os.environ["USERPROFILE"] + "\\Documents\\Rainmeter\\Skins\\Keep\\"
+
+# Move everything in the correct Rainmeter Skins folder for testing
+print(shutil.copytree(SOURCE, INSTALL_LOCATION, dirs_exist_ok=True))
+
+# Overwrite placeholder variables with debug ones
+config = configparser.ConfigParser()
+config.read("debug_settings.ini")
+with open(INSTALL_LOCATION + "@Resources\\Settings.inc") as configfile:
+    config.write(configfile)
