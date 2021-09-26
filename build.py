98184ed37c1cfadcb36c2b608ab708307e58e424
PrOF-kk <valerio.colella.2001@gmail.com>
2021-09-26 11:16:23 +0200

    Fix build script file permission

--- build.py
+++ build.py
@@ -14,5 +14,5 @@ print(shutil.copytree(SOURCE, INSTALL_LOCATION, dirs_exist_ok=True))
 # Overwrite placeholder variables with debug ones
 config = configparser.ConfigParser()
 config.read("debug_settings.ini")
-with open(INSTALL_LOCATION + "@Resources\\Settings.inc") as configfile:
+with open(INSTALL_LOCATION + "@Resources\\Settings.inc", "w") as configfile:
     config.write(configfile)
